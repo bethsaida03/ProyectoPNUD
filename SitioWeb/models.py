ffc275f418b5babdeed73d87e0b7b614b246c04e
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2023-03-06 17:04:51 -0500

    Models de producto y orden

--- SitioWeb/models.py
+++ SitioWeb/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Producto(models.Model):
+    imagen = models.ImageField( upload_to='productos', null=True)
+    nombre = models.CharField(max_length=150, )
+    descripcion = models.TextField(max_length=150, )
+    valor = models.IntegerField()
+    date_create = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+       
+        verbose_name_plural = "Producto"
+
+    def __str__(self):
+        return f' {self.nombre}, ${self.valor}-producto'
+
+
+class Orden (models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, blank=True, null=True)
+    cantidad =models.IntegerField(default=0, null=True, blank=True)
+    dato_agred = models.DateTimeField(auto_now_add=True)
