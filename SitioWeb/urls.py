3f7c3215d0a598659cf32272a05b27f27a95c448
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2023-03-06 17:10:20 -0500

    add urls

--- SitioWeb/urls.py
+++ SitioWeb/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     path('carrito', views.carrito, name='carrito'),
     path('registro', views.registro, name='registro'),
     path('iniciosesion', views.iniciosesion, name='iniciosesion'),
-    path('piedepagina', views.piedepagina, name='piedepagina'),
+    path('piedepagina', views.piedepagina, name='piedepagina'),path('producto', views.producto, name='producto'),
+    path('agregar/<int:producto_id>/', agregar_producto, name="Add"),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
+    path('restar/<int:producto_id>/', restar_producto, name="Dub"),
+    path('limpiar/', limpiar_carrito, name="CLS"), 
+    path('Carrito_compra', views.Carrito_compra, name='Carrito_compra'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
