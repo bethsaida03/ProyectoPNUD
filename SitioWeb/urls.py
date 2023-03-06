e0b7aeaf6304fc26c89a57d3592e5511352ca2c1
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2023-03-06 17:31:13 -0500

    modificar urls

--- SitioWeb/urls.py
+++ SitioWeb/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
 from . import views
 from .views import *
+from . import views
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.Home, name='home'),
     path('carrito', views.carrito, name='carrito'),
     path('registro', views.registro, name='registro'),
     path('iniciosesion', views.iniciosesion, name='iniciosesion'),
-    path('piedepagina', views.piedepagina, name='piedepagina'),path('producto', views.producto, name='producto'),
+    path('piedepagina', views.piedepagina, name='piedepagina'),
+    path('producto', views.producto, name='producto'),
     path('agregar/<int:producto_id>/', agregar_producto, name="Add"),
     path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
     path('restar/<int:producto_id>/', restar_producto, name="Dub"),
     path('limpiar/', limpiar_carrito, name="CLS"), 
-    
+    path('Carrito_compra', views.Carrito_compra, name='Carrito_compra'),
 ]
 
 if settings.DEBUG:
