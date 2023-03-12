167dad0de558aa4d935ff45b7dfac28aaaaa4d77
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2023-03-12 11:40:56 -0500

    Modificar models

--- SitioWeb/models.py
+++ SitioWeb/models.py
@@ -2,22 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Producto(models.Model):
-    imagen = models.ImageField( upload_to='productos', null=True)
-    nombre = models.CharField(max_length=150, )
-    descripcion = models.TextField(max_length=150, )
-    valor = models.IntegerField()
-    date_create = models.DateTimeField(auto_now=True)
+
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email= models.CharField(max_length=200, null=True)
+
+    def __str__(self) :
+        return self.name
     
-    class Meta:
-       
-        verbose_name_plural = "Producto"
 
-    def __str__(self):
-        return f' {self.nombre}, ${self.valor}-producto'
+class Producto(models.Model):
 
-
-class Orden (models.Model):
-    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, blank=True, null=True)
-    cantidad =models.IntegerField(default=0, null=True, blank=True)
-    dato_agred = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField( upload_to='productos', null=True)
+    nombre = model
