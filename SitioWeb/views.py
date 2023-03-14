fe29c85d46a8dc263bb6260270c222f4bb77f0d7
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2023-03-14 17:45:37 -0500

    update

--- SitioWeb/views.py
+++ SitioWeb/views.py
@@ -140,12 +140,14 @@ def updateItem(request):
     orderItem, created =OrderItem.objects.get_or_create(order= order, product=product)
 
     if action == 'add':
-        orderItem.quantity = (orderItem.quantity +1)
+        orderItem.quantity = (orderItem.quantity + 1)
     elif action == 'remove':
-        orderItem.quantity = (orderItem.quantity -1)
+        orderItem.quantity = (orderItem.quantity - 1)
 
     orderItem.save()
     if orderItem.quantity <= 0:
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
+
+
