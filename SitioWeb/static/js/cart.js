e703ee6533454aef2380178d208ebc12f2beed66
bethsaida03 <bethsaida.cheam@ug.edu.ec>
2023-03-14 17:55:48 -0500

    cambios

--- SitioWeb/static/js/cart.js
+++ SitioWeb/static/js/cart.js
@@ -1,13 +1,12 @@
 var updateBtns = document.getElementsByClassName('update-cart') 
 
-for (var i =0 ; i < updateBtns.length; i++){
+for (var i =  0 ; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
-        
-
         console.log('USER:', user)
+
         if (user === 'AnonymousUser'){
             console.log('Not logged in')
         }else {
